a3793b63d901f96b0bc2bbccad1c1babed01a934
panasenco <panasenco@users.noreply.github.com>
2019-12-26 13:52:48 -0800

    Added functions to quote identifiers and aggregate strings.

--- SqlTemplate.psm1
+++ SqlTemplate.psm1
@@ -6,16 +6,17 @@
     inline query, or a CREATE OR ALTER VIEW AS statement.
 .Parameter Path
     The path to the .sql file to convert (does not modify the file, just goes to stdout).
-.Parameter Server
-    The type of server to compile the query for. Allows the use of platform-agnostic EPS cmdlets like New-Concat,
-    New-StringAgg, and New-ToDate.
+.Parameter Binding
+    Hashtable containing value bindings to pass on to Invoke-EpsTemplate.
+    The 'Server' binding is mandatory - it is the type of server to compile the query for. The server binding allows
+    the use of platform-agnostic cmdlets like New-Concat, New-StringAgg, and New-ToDate in your EPS template files.
+.Parameter InlineReplace
+    SQL command where all instances of FROM <table> need to be replaced with FROM ({BODY}) {NAME}.
+    Recommended to be passed in through the pipeline.
 .Parameter CTE
     Set this switch to output in the CTE format {NAME} AS ({BODY}).
 .Parameter Inline
     Set this switch to output in the inline format ({BODY}) {NAME}.
-.Parameter InlineReplace
-    SQL command where all instances of FROM <table> need to be replaced with FROM ({BODY}) {NAME}.
-    Recommended to be passed in through the pipeline.
 .Parameter Prefix
     Set to script the query as a CREATE OR ALTER VIEW statement with the given fully qualified prefix.
     The prefix is prepended as is, and must include a trailing period if there is one.
@@ -29,35 +30,35 @@ function Use-SQL {
         [Parameter(Mandatory=$true, Position=0)]
         [string] $Path,
         [Parameter(Mandatory=$true)]
-        [ValidateSet("ORACLE", "SS13")]
-        [string] $Server,
-        [switch] $CTE,
-        [switch] $Inline,
+        [ValidateScript({$_.server -match 'ORACLE|SS\d+'})]
+        [Hashtable] $Binding,
         [Parameter(ValueFromPipeline=$true)]
         [string] $InlineReplace,
+        [switch] $CTE,
+        [switch] $Inline,
         [string] $Prefix,
         [switch] $Diff
     )
     # Apply the EPS template
-    $Body = Invoke-EpsTemplate -Path $Path -Binding @{Server=$Server}
+    $Body = $Binding | Invoke-EpsTemplate -Path $Path
 
     if ($CTE -or $Inline -or $InlineReplace -or $Prefix) {
         # Indent everything two spaces
         $Body = $Body -replace '^','  ' -replace "`n","`n  " -replace '  $'
         # Get the filebasename with lead non-alpha characters stripped
         $BaseName = [regex]::match($Path, '[^\\.]+(?=[^\\]*$)').Groups[0].Value -replace '^[\W\d_]*'
-        if ($CTE) {
+        if ($InlineReplace) {
+            # Replace the references to the view/query with inline expressions containing the query body.
+            $InlineReplace -replace "?<=((FROM|JOIN)\s+)$BaseName","(`r`n$Body) $BaseName"
+        } elseif ($CTE) {
             # Output CTE to be used in a WITH statement
             "$BaseName AS (`r`n$Body)"
         } elseif ($Inline) {
             # Output inline expression to be used with a FROM statement
             "(`r`n$Body) $BaseName"
-        } elseif ($InlineReplace) {
-            # Replace the references to the view/query with inline expressions containing the query body.
-            $InlineReplace -replace "?<=((FROM|JOIN)\s+)$BaseName","(`r`n$Body) $BaseName"
         } elseif ($Prefix) {
-            switch -regex ($Server) {
-                'SS\d+' { # Microsoft SQL Server '13
+            switch -regex ($Binding.Server) {
+                'SS\d+' { # Microsoft SQL Server
                     $Database, $Rest = $Prefix -split '\.',2
                     if ($Diff) {
                         "USE $Database`nGO`n$Body`nEXCEPT SELECT * FROM $Rest$BaseName"
@@ -73,11 +74,91 @@ function Use-SQL {
                     }
                 }
                 default {
-                    Write-Error "Unsupported server type: $Server."
+                    Write-Error "Unsupported server type: $Binding.Server."
                 }
             }
         }
     } else {
         $Body
+    }
+}
+
+<#
+.Synopsis
+    Quotes the given identifier with quoting characters appropriate for the given server.
+.Parameter Server
+    The server to quote the identifier for.
+.Parameter Name
+    The identifier name to quote.
+#>
+function New-QuotedId {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [ValidateScript({$_ -eq "ORACLE" -or $_ -match "SS\d+"})]
+        [string] $Server,
+        [Parameter(Mandatory=$true, Position=0)]
+        [string] $Name
+    )
+    $QuoteChars = switch -regex ($Server) {
+        'SS\d+' { '[',']' }
+        'ORACLE' { '"', '"' }
+        default { Write-Error "Server $Server not yet supported for identifier quoting" }
+    }
+    $QuoteChars[0] + $Name + $QuoteChars[1]
+}
+
+<#
+.Synopsis
+    Generates a string aggregation statement for grouped queries.
+.Parameter Server
+    The server to generate the aggregation statement for.
+.Parameter Expression
+    The SQL expression to aggregate.
+.Parameter GroupField
+    The fully qualified field to group by. The part after the last period is assumed to be the field name. The part
+    before the last period is assumed to be the fully qualified table name. Only mandatory for SQL Server '13.
+.Parameter Separator
+    The separator to use when aggregating.
+.Parameter Order
+    Order of aggregation.
+#>
+function New-StringAgg {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [ValidateSet("ORACLE", "SS13")]
+        [string] $Server,
+        [Parameter(Mandatory=$true, Position=0)]
+        [string] $Expression,
+        [string] $GroupField = '',
+        [string] $Separator = '',
+        [string] $Order = '1'
+    )
+    switch ($Server) {
+        'SS13' {
+            if ($GroupField) {
+                $Table = ($GroupField | Select-String -Pattern '.*(?=\.[^\.]*)').Matches[0].Value
+                $Field = ($GroupField | Select-String -Pattern '[^\.]*$').Matches[0].Value
+            } else {
+                Write-Error 'GroupField param mandatory for SQL Server 13'
+            }
+@"
+STUFF((
+      SELECT
+        N'$Separator' + $Expression
+      FROM $Table t2
+      WHERE $Table.$Field=t2.$Field
+      ORDER BY $Order
+      FOR XML PATH (N'')
+    ), 1, $($Separator.Length), N'') 
+"@
+        }
+        'ORACLE' {
+            "LISTAGG($Expression, '$Separator') WITHIN GROUP (ORDER BY $Order)"
+        }
+        default {
+            Write-Error "Server $Server not yet supported for list aggregation"
+        }
     }
 }
