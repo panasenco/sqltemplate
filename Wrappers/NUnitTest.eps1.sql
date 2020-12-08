657cc6bba2b1e5d84818bc5331959a49e520dc51
panasenco <panasenco@users.noreply.github.com>
2020-12-08 14:51:17 -0800

    Renamed TableName to GroupName in NUnitTest

--- Wrappers/NUnitTest.eps1.sql
+++ Wrappers/NUnitTest.eps1.sql
@@ -1,12 +1,12 @@
 <%#
 .Synopsis
     NUnit XML wrapper for a single test case.
-.Binding TableName
-    Name of the table being tested.
+.Binding GroupName
+    Name of the test group (e.g. table being tested).
 .Binding TestName
     Name of the test case.
 -%>
-<%- $TableName = if ($TableName) {$TableName -replace '\.'} else {'customquery'} -%>
-<%= $Binding | Invoke-SqlTemplate -Template ("'<test-case name=" + '"sqltest.' + $TableName + '.' +
+<%- $GroupName = if ($GroupName) {$GroupName -replace '\.'} else {'customquery'} -%>
+<%= $Binding | Invoke-SqlTemplate -Template ("'<test-case name=" + '"sqltest.' + $GroupName + '.' +
     ($TestName -replace '\.') + '" executed="True" success="' + "'`r`n" +
     "CASE WHEN $Body THEN 'True' ELSE 'False' END`r`n" + "'" + '"' + "/>'") -Wrapper 'Concatenate' %> AS test_result
